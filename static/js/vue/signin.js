49c986a16368c2b7180191fa16c083a6cec909c7
aaaasmile <testhaus@gmx.at>
2020-04-04 23:25:46 +0200

    update

--- static/js/vue/signin.js
+++ static/js/vue/signin.js
@@ -2,7 +2,8 @@ export const SignIn = Vue.component('signin', {
   data() {
     return {
       username: '',
-      password: ''
+      password: '',
+      show2: false
     }
   },
   methods: {
@@ -20,16 +21,35 @@ export const SignIn = Vue.component('signin', {
   template: `
   <div>
     <h2>Sign In</h2>
-    <div>
-      <div>
-        <label for="username">Username</label>
-        <input id="username" v-model="username" type="text" name="username" />
-      </div>
-      <div>
-        <label for="password">Password</label>
-        <input id="password" v-model="password" type="password" name="password" />
-      </div>
-      <button v-on:click="SignIn">Sign In</button>
-    </div>
+    <v-form>
+      <v-container>
+        <v-row>
+          <v-col cols="12" sm="6" md="3">
+            <v-text-field
+              label="Username"
+              id="username"
+              v-model="username"
+              type="text"
+              name="username"
+            ></v-text-field>
+          </v-col>
+          <v-col cols="12" sm="6" md="3">
+            <v-text-field
+              :append-icon="show2 ? 'mdi-eye' : 'mdi-eye-off'"
+              :type="show2 ? 'text' : 'password'"
+              name="password"
+              id="password"
+              v-model="password"
+              label="Password"
+              hint="At least 6 characters"
+              class="input-group--focused"
+              @click:append="show2 = !show2"
+            ></v-text-field>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-form>
+    <v-icon dark>folder_open</v-icon>
+    <v-btn v-on:click="SignIn">Sign In</v-btn>
   </div>`
 })
--- static/js/vue/signin.vue
+++ static/js/vue/signin.vue
@@ -1,16 +1,35 @@
 <template>
   <div>
     <h2>Sign In</h2>
-    <div>
-      <div>
-        <label for="username">Username</label>
-        <input id="username" v-model="username" type="text" name="username" />
-      </div>
-      <div>
-        <label for="password">Password</label>
-        <input id="password" v-model="password" type="password" name="password" />
-      </div>
-      <button v-on:click="SignIn">Sign In</button>
-    </div>
+    <v-form>
+      <v-container>
+        <v-row>
+          <v-col cols="12" sm="6" md="3">
+            <v-text-field
+              label="Username"
+              id="username"
+              v-model="username"
+              type="text"
+              name="username"
+            ></v-text-field>
+          </v-col>
+          <v-col cols="12" sm="6" md="3">
+            <v-text-field
+              :append-icon="show2 ? 'mdi-eye' : 'mdi-eye-off'"
+              :type="show2 ? 'text' : 'password'"
+              name="password"
+              id="password"
+              v-model="password"
+              label="Password"
+              hint="At least 6 characters"
+              class="input-group--focused"
+              @click:append="show2 = !show2"
+            ></v-text-field>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-form>
+    <v-icon dark>folder_open</v-icon>
+    <v-btn v-on:click="SignIn">Sign In</v-btn>
   </div>
 </template>
